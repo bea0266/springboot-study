fea43c618dba8140454ca9c4256c3941e906c90b
bea0266 <bea0266@naver.com>
2022-06-30 00:52:33 +0900

    jpa, mariadb(jdbc) 연동

--- src/main/java/com/springboot/api/user/controller/UserController.java
+++ src/main/java/com/springboot/api/user/controller/UserController.java
@@ -7,9 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    /*
+     * user를 컨트롤할 수 있는 컨트롤러를 만들었다.
+     * webpage를 return 하지 않고 User(Object)를 response body에 return 한다.
+     */
     @GetMapping("/user")
     public Object getUser() {
-        User user = User.builder().id("bsc1234").name("baeseungchan").build();
+        User user = User.builder().userId("bsc1234").name("baeseungchan").build();
         return user;
+
     }
 }
--- src/main/java/com/springboot/api/user/domain/User.java
+++ src/main/java/com/springboot/api/user/domain/User.java
@@ -6,16 +6,33 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
 
+@Entity
+@Table(name = "user")
+@DynamicInsert
+@DynamicUpdate
 @Getter
 @Setter
 @Builder
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class User {
-    public String id;
-    public String name;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    public Date createdDate;
+
+    @Column(name = "USER_ID", nullable = false)
+    private String userId;
+    @Column(name = "PASSWORD", nullable = false)
+    private String password;
+    @Column(name = "NAME", nullable = false)
+    private String name;
+    @Column(name = "ADDRESS", nullable = true)
+    private String address;
+
 }
